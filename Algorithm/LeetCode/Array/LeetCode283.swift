88f1ac30531a5849e374fad43b967d80baa4654f
ReverseScale <ReverseScale@iCloud.com>
2019-03-22 12:47:53 +0800

    ✨:Move Zeroes 283.

---
+++ Algorithm/LeetCode/Array/LeetCode283.swift
@@ -0,0 +1,45 @@
+//
+//  LeetCode01.swift
+//  LeetCode
+//
+//  Created by Steven Xie on 2019/2/18.
+//  Copyright © 2019 Steven Xie. All rights reserved.
+//
+
+extension ViewController {
+    
+    /*
+     283. 移动零
+     给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     
+     示例:
+     
+     输入: [0,1,0,3,12]
+     输出: [1,3,12,0,0]
+     说明:
+     
+     必须在原数组上操作，不能拷贝额外的数组。
+     尽量减少操作次数。
+     */
+    
+    func moveZeroes(_ nums: inout [Int]) {
+        let count = nums.count
+        nums = nums.filter { $0 != 0 }
+        let newCount = nums.count
+        for _ in 0..<count - newCount {
+            nums.append(0)
+        }
+    }
+    
+    func moveZeroes02(_ nums: inout [Int]) {
+        var j = 0
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                let temp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = temp
+                j += 1
+            }
+        }
+    }
+}
